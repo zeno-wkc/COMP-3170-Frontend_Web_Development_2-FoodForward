86912303d10a96ea0b790101ff8b8ce1d7ad69b9
Zeno <zenowkc@gmail.com>
2024-10-20 15:56:12 -0700

    Update search bar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import SearchIcon from './SearchIcon';
+import UserIcon from './UserIcon';
+import './SearchBar.css';
+
+function SearchBar() {
+  return (
+    <div className='search-bar__container'>
+      <div className='search-bar__content'>
+        <form className='search-bar__from'>
+          <label><SearchIcon size='24' color='#2E2217'/></label>
+          <input type='text' placeholder='Search'></input>
+        </form>
+        <button className='account-btn'><UserIcon size='24' color='#2E2217'/></button>
+      </div>
+    </div>
+  )
+}
+
+export default SearchBar
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import SearchBar from '../components/SearchBar';
 
 function Home() {
-  return <h2>Home Page</h2>;
+  return (
+    <>
+      <h2>Home Page</h2>
+      <SearchBar />
+      <p style={{color: "#000"}}>skjslajld</p>
+    </>
+  ); 
 }
 
 export default Home;
